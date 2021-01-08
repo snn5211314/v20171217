bce2538f94c23f0242daa393503529cb365d122c
zhangteng <18829282822@163.com>
2021-01-08 12:36:53 +0800

    MVVM原理学习

--- vue-stage/dist/vue.js
+++ vue-stage/dist/vue.js
@@ -6,6 +6,8 @@
 
   function Vue(options) {  // 暴露给外部的方法
     console.log(options);
+    // 将options挂载在Vue上变成私有属性
+    this.$options = options;
   }
 
   return Vue;
--- vue-stage/src/index.js
+++ vue-stage/src/index.js
@@ -1,5 +1,7 @@
 function Vue(options) {  // 暴露给外部的方法
   console.log(options)
+  // 将options挂载在Vue上变成私有属性
+  this.$options = options
 }
 
 export default Vue
